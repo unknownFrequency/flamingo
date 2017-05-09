e6f22a407f97125a76e983eec1ab574ca7cdaf2c
Ruben Thuesen <unknownfreq@protonmail.ch>
2017-05-09 20:50:00 +0200

    added tasks

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Flamingo</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+    <h2>Opgaver</h2>
+    <ul>
+        @foreach ($tasks as $task)
+            <li><a href="/tasks/{{$task->id}}">{{ $task->title }}</a></li>
+            <li>{{ $task->body }}</li>
+            <br />
+        @endforeach
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,15 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/tasks', function () {
+    $tasks = DB::table('tasks')->get();
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function ($id) {
+    $task = DB::table('tasks')->find($id);
+    //dd($task);
+    return view('tasks/show', compact('task'));
+});
+
