6eb264a7e6fa82021ef5b9432c7b9a22ef6c9694
Ruben Thuesen <unknownfreq@protonmail.ch>
2017-05-13 21:04:11 +0200

    added auth

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
-// Post / Artikler siden
+// Post sider
 Route::get('/posts', 'PostsController@index');
 Route::get('/post/create', 'PostsController@create');
 Route::get('/post/{post}', 'PostsController@show');
 Route::post('/post', 'PostsController@store');
 
 // Forsiden
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('welcome'); });
+
+Auth::routes();
+
+// For logged in users
+Route::get('/home', 'HomeController@index')->name('home');
