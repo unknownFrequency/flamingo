6e321dca12331a6884b936c64a8ba9c228c3a91a
Ruben Thuesen <unknownfreq@protonmail.ch>
2017-05-23 13:28:03 +0200

    bootstrap4 working

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,1 @@
-<!doctype html>
-<html lang="{{ config('app.locale') }}">
-
-@include('include/head')
-
-<body id="home" class="homepage">
-
-<!-- Include Navigation -->
-@include('include/navigation')
-
-</body>
-</html>
-
+@extends('include/layout')
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,13 @@
 const mix  = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js([
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/tether/dist/js/tether.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.js',
+    'resources/assets/js/app.js'
+], 'public/js/app.js')
+    .sass('resources/assets/sass/app.scss', 'public/css/app.css');
+mix.autoload({
+    jquery: ['$', 'jQuery', 'window.jQuery'],
+    tether: ['Tether', 'window.Tether']
+});
