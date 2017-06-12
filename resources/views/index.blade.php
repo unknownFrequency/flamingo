4701fc784e9ca0861125ec71bd72f00c310bca43
ajhshop <aj@hshop.dk>
2017-06-12 13:31:00 +0200

    tabs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -55,13 +55,6 @@
             <h2> En fed overskrift </h2>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
           </div>
-            <h2> Produkter </h2>
-              <a class="btn btn-primary" href="#">Produkt</a>
-              <a class="btn btn-primary" href="#">Produkt</a>
-              <a class="btn btn-primary" href="#">Produkt</a>
-              <a class="btn btn-primary" href="#">Produkt</a>
-              <a class="btn btn-primary" href="#">Produkt</a>
-          </div>
         </div>
       </section>
       <section id="section-iconbox-2">
