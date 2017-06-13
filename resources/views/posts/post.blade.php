c166ea12217e6abac9731395f6e8d17f10919d02
Ruben Thuesen <unknownfreq@protonmail.ch>
2017-06-13 15:35:50 +0200

    set default values for all entries except body

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,22 +1,24 @@
-<h2>
-    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-</h2>
+@if($post->status === "PUBLISHED")
+    <h2>
+        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+    </h2>
 
-<p class="lead">
-    af: <strong>{{ App\User::find($post->author_id)->name }}</strong>
-</p>
+    <p class="lead">
+        af: <strong>{{ App\User::find($post->author_id)->name }}</strong>
+    </p>
 
-<p>
-    den {{ $post->created_at->toFormattedDateString() }}
-</p>
+    <p>
+        d. {{ $post->created_at->toFormattedDateString() }}
+    </p>
 
-<p >
-    <img style="width: 400px; height:300px;" src="{{ $post->image }}" />
-</p>
+    <p>
+        <img style="width: 400px; height:300px;" src="{{ $post->image }}" />
+    </p>
 
-<p>
-    <a href="/posts/{{ $post->id }}">
-        {{ $post->excerpt }}
-    </a>
-</p>
-<hr />
+    <p>
+        <a href="/posts/{{ $post->id }}">
+            {{ $post->excerpt }}
+        </a>
+    </p>
+    <hr />
+@endif
