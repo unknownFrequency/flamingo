0908faf97135e9d60e6a6b08a6a2fe6d1f935ca0
ajhshop <aj@hshop.dk>
2017-06-14 13:30:54 +0200

    Mobil Layout

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -3,10 +3,9 @@
         <div class="row">
             <div class="col-sm-3">
                 <h2 class="logo"><a href="#"><span class="header-highlight">Flamingo</span> #Media</a></h2>
-                <h4 class="widget-title">Følg os på de sociale medier</h4>
+                <h4 class="widget-title">Følg os:</h4>
                  <ul class="social-nav">
                      <li><a href="#" target="_blank" title="Facebook" rel="nofollow" class="facebook"><i class="fa fa-facebook"></i></a></li>
-                     <li><a href="#" target="_blank" title="Google plus" rel="nofollow" class="google"><i class="fa fa-google-plus"></i></a></li>
                      <li><a href="#" target="_blank" title="Linkedin" rel="nofollow" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
                  </ul>
             </div>
