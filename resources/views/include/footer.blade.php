9709f593dffbf6846172be44b122bb27784b97e5
ajhshop <aj@hshop.dk>
2017-06-14 14:17:23 +0200

    Footer

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-3">
-                <h2 class="logo"><a href="#"><span class="header-highlight">Flamingo</span> #Media</a></h2>
+                <h2 class="logo"><span class="header-highlight">Flamingo</span> #Media</h2>
                 <h4 class="widget-title">Følg os:</h4>
                  <ul class="social-nav">
                      <li><a href="#" target="_blank" title="Facebook" rel="nofollow" class="facebook"><i class="fa fa-facebook"></i></a></li>
@@ -19,12 +19,14 @@
             <div class="col-sm-3">
                 <h5>Kontaktinformation</h5>
                     <p>Flamingomedia Aps<br>P. Hiort-Lorenzens Vej 4<br>8000 Aarhus C</p>
+                    <p>Telefon: <a href="tel:+4556568181">+45 56 56 81 81</a></p>
+                    <p>Email: <a href="mailto:info@flamingomedia.dk?Subject=Hej%20Flamingoer" target="_top">info@flamingomedia.dk</a></p>
 
             </div>
             <div class="col-sm-3">
                 <div>
                   <h5>Bliv Kontaktet</h5>
-                  <p> tekst om svartid blabla </p>
+                  <p> Vi kontakter dig indenfor 24 timer. </p>
                     @include('include/sms_form')
                 </div>
 
