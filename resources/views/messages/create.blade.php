91ad2a96ee5d53e6a7d07850dc59f3a92ad4417c
Ruben Thuesen <unknownfreq@protonmail.ch>
2017-06-20 13:54:30 +0200

    Viser link til beskeder i admin forside

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -1,11 +1,10 @@
 @extends('include/layout')
 
 @section('content')
-    <h1>Skriv en besked til FLamingo</h1>
+    <h1>Skriv en besked til Flamingos Team</h1>
 
     @include('messages/include/form')
 
     @include('include/errors')
-
 
 @endsection
