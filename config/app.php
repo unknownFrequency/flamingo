6bc05faad7dfe7a6c7b92b37f28dacbf3aaf826e
Ruben Thuesen <unknownfreq@protonmail.ch>
2017-08-11 15:48:39 +0200

    tilføjet form knap 'Færdiggør' til admin.index

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
 
         /*
@@ -228,9 +229,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-//        'Twilio' => 'Aloha\Twilio\Support\Laravel\Facade',
         'Twilio' => Aloha\Twilio\Support\Laravel\Facade::class,
         'Mailgun' => Bogardo\Mailgun\Facades\Mailgun::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
