c04e80b4ff133ce8daf1b0b4d3c419bbe9370082
Ruben Thuesen <unknownfreq@protonmail.ch>
2017-08-22 09:21:55 +0200

    X for closing modal back on

--- resources/views/include/contact_modal.blade.php
+++ resources/views/include/contact_modal.blade.php
@@ -1,10 +1,11 @@
 <!-- The Modal -->
-<div id="myModal" class="modal" role="dialog">
+<div id="myModal" class="modal" role="dialog"
+     aria-labelledby="contact form" aria-hidden="true">
     <!-- Modal content -->
     <div class="modal-content">
-        {{--<div class="modal-header">--}}
-            {{--<span class="close">&times;</span>--}}
-        {{--</div>--}}
+        <div class="modal-header">
+            <span class="close">&times;</span>
+        </div>
         <div class="modal-body">
             <div class="row">
                 <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" style="padding:20px;">
@@ -15,12 +16,14 @@
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" style="padding:20px;">
-                    <p class="lead">Få et uforpligtende tilbud <span class="text-success">indenfor 24 timer</span></p>
+                    <p class="lead">Få et uforpligtende tilbud
+                        <span class="text-success">indenfor 24 timer</span>
+                    </p>
                     <ul class="list-unstyled" style="line-height: 2">
                         <li><span class="fa fa-check text-success"></span> See all your orders</li>
                         <li><span class="fa fa-check text-success"></span> Fast re-order</li>
                         <li><span class="fa fa-check text-success"></span> Save your favorites</li>
-                        <img class="img-responsive" src="{{asset('images/tilbud.png')}}" alt="">
+                        <img class="img-responsive" src="{{asset('images/tilbud.png')}}" alt="Tilbud">
                     </ul>
                 </div>
             </div>
